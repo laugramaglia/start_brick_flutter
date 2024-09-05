5b1a3439a855c2117ab587f2d56f7d569e410581
Laureano <laugramaglia@gmail.com>
2024-09-05 18:04:21 -0300

    Add files via upload

---
+++ __brick__/lib/core/theme/theme_hive/hive_adapters.dart/locale_adapter.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:squad_rush/core/config/local_db/const_type_id.dart';
+
+class LocaleAdapter extends TypeAdapter<Locale> {
+  @override
+  final int typeId = kLocaleTypeId;
+
+  @override
+  Locale read(BinaryReader reader) {
+    final languageCode = reader.readString();
+    final countryCode = reader.readString();
+    return Locale(languageCode, countryCode.isNotEmpty ? countryCode : null);
+  }
+
+  @override
+  void write(BinaryWriter writer, Locale obj) {
+    writer.writeString(obj.languageCode);
+    writer.writeString(obj.countryCode ?? '');
+  }
+}
